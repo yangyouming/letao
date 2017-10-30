4ff16433d2c6743614bcb8144340537aac493c29
yangym <1401552538@qq.com>
2017-10-30 21:20:48 +0800

    后台管理系统登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,11 +1,14 @@
 /**
  * Created by 杨友明 on 2017/10/29.
  */
-$(function () {
 
+
+
+
+$(function () {
+  var $form=$("#form");
   //1初始化表单插件
-  var $from=$("#from");
-  $from.bootstrapValidator({
+  $form.bootstrapValidator({
     feedbackIcons: {
       //校验时使用的图标
       valid: 'glyphicon glyphicon-ok',
@@ -43,6 +46,38 @@ $(function () {
     }
   });
 
+  var validator = $form.data('bootstrapValidator');  //获取表单校验实例
 
+  //表单校验成功时,会触发success.from.bv事件,此时会提交表单,我们需要静止表单提交,使用ajax提交表单
+  $form.on('success.form.bv', function (e) {
+    e.preventDefault();
 
+    //发送ajax请求,以为之二我们需要获取username和password的值
+    $.ajax({
+      type:"post",
+      url:" /employee/employeeLogin",
+      data:$form.serialize(),
+      success:function (data) {
+        if(data.success){
+          location.href="index.html";
+        }else {
+          if (data.error===1000){
+            validator.updateStatus('username', 'INVALID', 'callback');
+          }
+          if (data.error===1001){
+            validator.updateStatus('password', 'INVALID', 'callback');
+          }
+        }
+      }
+
+    })
+  });
+
+  //表单重置功能
+  $("[type='reset']").on("click",function () {
+    //调用表单重置功能
+    //获取表单校验实例,调取resetForm方法
+
+    validator.resetForm();//重置表单，并且会隐藏所有的错误提示和图标
+  })
 });
