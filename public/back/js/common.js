0ea57698b4a9dd15ed58ee2ea1d173a9458facec
yangym <1401552538@qq.com>
2017-10-30 22:55:55 +0800

    登录功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,16 @@
 /**
  * Created by 杨友明 on 2017/10/29.
  */
+//希望在ajax开始之前显示进度条 NProgress.start();
+// NProgress.start();
+
+$(document).ajaxStart(function () {
+  //让进度条显示
+  NProgress.start();
+});
+$(document).ajaxStart(function () {
+  setTimeout(function () {
+    //让精度条结束
+    NProgress.done();
+  },500)
+});
