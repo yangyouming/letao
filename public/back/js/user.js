22202cb5011100a3c20cc6aee3c39ae1783ee15f
yangym <1401552538@qq.com>
2017-10-31 21:49:30 +0800

    用户管理模块完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -34,4 +34,32 @@ $(function () {
   }
   render();
 
+  //点击启用或禁用按钮弹出模态框
+  //这里的按钮都是动态渲染迟来的,所以我们
+  $("tbody").on("click",".btn",function () {
+    $("#userModal").modal("show");
+    var id=$(this).parent().data("id");
+    var isDelete=$(this).parent().data("isDelete");
+    isDelete = isDelete === 1 ? 0 : 1;
+    //点击确定按钮,需要启用或禁用按钮
+    $(".btn_confrim").off().on("click",function () {
+
+      //发送ajax请求
+      $.ajax({
+        type:"post",
+        url:" /user/updateUser",
+        data:{
+          id:id,
+          isDelete:isDelete
+        },
+        success:function (data) {
+          if(data.success){
+            $("#userModal").modal("hide");
+            render();
+          }
+        }
+      })
+    })
+  })
+
 });
